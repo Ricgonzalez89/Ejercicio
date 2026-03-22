50844d88605ee3c033d6c33ba1699d817560f453
Ricgonzalez89 <ricardoa.gonzalez04@gmail.com>
2026-03-21 22:00:45 -0400

    Inclusion de clase descriptora

--- Cl_persona.py
+++ Cl_persona.py
@@ -1,10 +1,39 @@
+class Cl_personaDescriptor:
+     
+    def __set_name__(self, owner, name):
+         self.nombre_publico = name
+         self.nombre_privado = "_" + name
+    
+    def __get__(self, instance, owner=None):
+       print(f"Getter llamado para el atributo: {self.nombre_publico}")
+       valor = getattr(instance, self.nombre_privado)
+       return valor
+    
+    def __set__(self, instance, value):
+       print(f"Setter llamado para el atributo: {self.nombre_publico}")
+       setattr(instance, self.nombre_privado, value)
+
+    def __delete__(self, instance):
+        print(f"Delete llamado para el atributo {self.nombre_publico}")
+        delattr(instance, self.nombre_privado)
+
 class Cl_persona:
-    def __init__(self, nombre, apellido):
+
+    nombre = Cl_personaDescriptor()
+    apellido = Cl_personaDescriptor()
+    edad = Cl_personaDescriptor()
+
+    def __init__(self, nombre, apellido, edad):
         self.nombre = nombre
         self.apellido = apellido
+        self.edad = edad
 
     def saludo(self):
-            print(f"¡Hola!, yo soy {self.nombre} {self.apellido}")
+            print(f"¡Hola!, yo soy {self.nombre} {self.apellido} y tengo {self.edad} años.")
     
-#per = Cl_persona("Ricardo", "González")
-#per.saludo()
+per = Cl_persona("Ricardo", "González", 21)
+per.saludo()
+
+print(per.nombre)
+del per.edad
+print(vars(per))
